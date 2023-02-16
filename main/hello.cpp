63114693d86c8881f468bf2dda2837a4d1479a86
divya820 <79748343+divya820@users.noreply.github.com>
2023-02-17 00:01:33 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "PES2UG2CS114>> Successfully modified cpp file\n";
   return 0;
 }
